f9b8732bbd5bb827d5fe3fc093d8d457e7abe26c
BigBoyDevBox <BigBoyDevBox@users.noreply.github.com>
2026-08-27 15:24:18 +0000

    Drop the leftover detectTapGestures import

--- app/src/main/java/ch/lkmc/bangnidraw/ui/canvas/CanvasScreen.kt
+++ app/src/main/java/ch/lkmc/bangnidraw/ui/canvas/CanvasScreen.kt
@@ -21,7 +21,6 @@ import androidx.compose.animation.core.spring
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.gestures.awaitEachGesture
-import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
